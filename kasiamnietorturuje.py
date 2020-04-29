6ca0aeb43bafe0308bd7957bcf0402e54e5215ea
Funky_Krova <marten.jarosz@gmail.com>
2020-04-29 23:04:59 +0200

    added 2nd argument to i_ate - str and int lesson

--- kasiamnietorturuje.py
+++ kasiamnietorturuje.py
@@ -6,3 +6,16 @@ def i_ate(food):
 
 def multiply(a,b):
     return a*b
+
+def i_ate(food,a):
+    #not correct/not complete solution
+    #print("I ate too much " + food + ", should have eaten ")
+    #print(a/2)
+
+    #without rounding - str
+    ammount = str(a / 2)
+    print("I ate too much " + food + ", should have eaten " + ammount)
+    
+    #wih rounding - change to int
+    #ammount = str(int(a / 2))
+    #print("I ate too much " + food + ", should have eaten " + ammount)
