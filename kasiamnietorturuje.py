42e2f7804586da6026e13e13fc8481554ba18ada
Funky_Krova <marten.jarosz@gmail.com>
2020-04-29 23:41:44 +0200

    added list and for functions

--- kasiamnietorturuje.py
+++ kasiamnietorturuje.py
@@ -15,7 +15,16 @@ def i_ate(food,a):
     #without rounding - str
     ammount = str(a / 2)
     print("I ate too much " + food + ", should have eaten " + ammount)
-    
+
     #wih rounding - change to int
     #ammount = str(int(a / 2))
     #print("I ate too much " + food + ", should have eaten " + ammount)
+
+def dont_eat(list):
+    for x in list: print("Don't eat " + x)
+    #for x in zakazane: print(x + "!")
+    #niezdrowe = ["oliwki", "szpinak", "owsianka"] - przyklad listy
+
+def do_eat(a, list):
+    ammount = str(a)
+    for x in list: print("Do eat " + ammount + " " + x)
